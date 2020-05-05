70e21f9c1954ab5e5bc48b14ca0af79228d929ad
whosuka <zhekaurab@gmail.com>
2020-05-05 17:43:01 +0300

    new version of main according to new database paradigm.

--- main.py
+++ main.py
@@ -24,30 +24,16 @@ Builder.load_file("node_editor_screen.kv")
 
 
 class SkillHelperScreenManager(ScreenManager):
-    def __init__(self, db_skills, db_tasks, **kwargs):
-        self.db_skills = db_skills
-        self.db_tasks = db_tasks
+    def __init__(self, database, **kwargs):
+        self.database = database
         super(SkillHelperScreenManager, self).__init__(**kwargs)
 
 
 class SkillHelperApp(App):
     def build(self):
         # link database and interface
-        db_skills = Database("skills")
-        db_tasks = Database("tasks")
-        sm = SkillHelperScreenManager(db_skills=db_skills, db_tasks=db_tasks)
-
-        db_skills.view_screen = sm.skills_screen
-        db_skills.info_screen = sm.skill_info_screen
-        db_skills.edit_screen = sm.edit_skill_screen
-        db_skills.group_screen = sm.group_screen
-
-        db_tasks.view_screen = sm.tasks_screen
-        db_tasks.info_screen = sm.task_info_screen
-        db_tasks.edit_screen = sm.edit_task_screen
-        # initialize view
-        db_skills.refresh_stats()
-        db_tasks.refresh_stats()
+        db = Database("skills")
+        sm = SkillHelperScreenManager(db)
 
         sm.current = "group_screen"
         return sm
